90fd9343119cf9fbd12ea5a869a31c4ee84a78a2
Sneha Gangwar <snehagangwarbhr@gmail.com>
2025-09-19 23:58:18 +0530

    Create fibonacci.java

---
+++ fibonacci.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+     long num = n;
+    if(num==0 || num ==1) 
+        return (int)(num);
+    else{
+        for(long i=2;i<num;i++){
+            
+        }
+    }
+
+    }
+}
